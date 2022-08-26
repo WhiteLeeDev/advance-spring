f0bd34fdda7c563edafa194db8522e8a58c02a0e
dg1.lee <akxkdldhtm@gmail.com>
2022-08-26 23:10:04 +0900

    second commit

---
+++ src/main/java/org/advancespring/LogTracerApplication.java
@@ -0,0 +1,13 @@
+package org.advancespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogTracerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogTracerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/advancespring/LogTracerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.advancespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogTracerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
