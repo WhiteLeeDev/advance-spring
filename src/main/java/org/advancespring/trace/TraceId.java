d9216548aafd87b9604bd5a16e694e000d6f3e8d
dg1.lee <akxkdldhtm@gmail.com>
2022-08-26 23:26:59 +0900

    [Implementation] 구현완료, 테스트 성공

--- src/main/java/org/advancespring/trace/TraceId.java
+++ src/main/java/org/advancespring/trace/TraceId.java
@@ -1,19 +1,38 @@
 package org.advancespring.trace;
 
+import java.util.UUID;
+
 public class TraceId {
+    private final int level;
+    private final String id;
+
+    public TraceId(){
+        this.level = 0;
+        this.id = createId();
+    }
+
+    private TraceId(String id, int level) {
+        this.id = id;
+        this.level = level;
+    }
+
+    private String createId() {
+        return UUID.randomUUID().toString().substring(0,8);
+    }
+
     public int getLevel() {
-        return 0;
+        return this.level;
     }
 
     public String getId() {
-        return "";
+        return this.id;
     }
 
     public TraceId createNextId() {
-        return null;
+        return new TraceId(this.id,this.level + 1);
     }
 
     public TraceId createPreviousId() {
-        return null;
+        return new TraceId(this.id,this.level -1);
     }
 }
